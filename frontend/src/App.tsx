c7656d25c594989011f969ecb7b1b11404c8644b
samkakade25 <samkakade44@gmail.com>
2025-08-13 00:59:37 +0530

    Fix: frontend deployment error resolved with removing unused pages in app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,8 @@
 import type { ReactElement } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Drivers from "./pages/Drivers";
-import RoutesPage from "./pages/RoutesPage";
-import Orders from "./pages/Orders";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
-// import Dashboard from "./pages/Dashboard";
 import SimulationPage from "./pages/SimulationPage";
 import DashboardPage from "./pages/SimulationDashboard";
 
@@ -25,9 +21,6 @@ export default function App() {
 
         <Route path="/dashboard" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
         <Route path="/simulation" element={<PrivateRoute><SimulationPage /></PrivateRoute>} />
-        <Route path="/drivers" element={<PrivateRoute><Drivers /></PrivateRoute>} />
-        <Route path="/routes" element={<PrivateRoute><RoutesPage /></PrivateRoute>} />
-        <Route path="/orders" element={<PrivateRoute><Orders /></PrivateRoute>} />
       </Routes>
     </BrowserRouter>
   );
